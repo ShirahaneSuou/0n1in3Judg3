ec2d8f9a5a4ccdcad65b1c39fdf00ede33d915c3
Lweleth <SoungEarlf@gmail.com>
2018-12-21 20:07:31 +0800

    add limit

---
+++ dump-judge-201812201737.sql
@@ -0,0 +1,513 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: judge
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `VJ_Solution`
+--
+
+DROP TABLE IF EXISTS `VJ_Solution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `VJ_Solution` (
+  `runid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `lang` int(10) unsigned NOT NULL DEFAULT '0',
+  `code` text NOT NULL,
+  `user_id` char(20) NOT NULL,
+  `status` char(20) NOT NULL,
+  PRIMARY KEY (`runid`),
+  KEY `uid` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `VJ_Solution`
+--
+
+LOCK TABLES `VJ_Solution` WRITE;
+/*!40000 ALTER TABLE `VJ_Solution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `VJ_Solution` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compileinfo`
+--
+
+DROP TABLE IF EXISTS `compileinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `compileinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `error` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compileinfo`
+--
+
+LOCK TABLES `compileinfo` WRITE;
+/*!40000 ALTER TABLE `compileinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compileinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contest`
+--
+
+DROP TABLE IF EXISTS `contest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contest` (
+  `contest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `description` text,
+  `private` tinyint(4) NOT NULL DEFAULT '0',
+  `langmask` int(11) NOT NULL DEFAULT '0' COMMENT 'bits for LANG to mask',
+  `password` char(16) NOT NULL DEFAULT '',
+  PRIMARY KEY (`contest_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contest`
+--
+
+LOCK TABLES `contest` WRITE;
+/*!40000 ALTER TABLE `contest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contest_problem`
+--
+
+DROP TABLE IF EXISTS `contest_problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contest_problem` (
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `contest_id` int(11) DEFAULT NULL,
+  `title` char(200) NOT NULL DEFAULT '',
+  `num` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contest_problem`
+--
+
+LOCK TABLES `contest_problem` WRITE;
+/*!40000 ALTER TABLE `contest_problem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contest_problem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loginlog`
+--
+
+DROP TABLE IF EXISTS `loginlog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loginlog` (
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(40) DEFAULT NULL,
+  `ip` varchar(100) DEFAULT NULL,
+  `time` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loginlog`
+--
+
+LOCK TABLES `loginlog` WRITE;
+/*!40000 ALTER TABLE `loginlog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loginlog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mail`
+--
+
+DROP TABLE IF EXISTS `mail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mail` (
+  `mail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_user` varchar(20) NOT NULL DEFAULT '',
+  `from_user` varchar(20) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` text,
+  `new_mail` tinyint(1) NOT NULL DEFAULT '1',
+  `reply` tinyint(4) DEFAULT '0',
+  `in_date` datetime DEFAULT NULL,
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`mail_id`),
+  KEY `uid` (`to_user`)
+) ENGINE=MyISAM AUTO_INCREMENT=1013 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mail`
+--
+
+LOCK TABLES `mail` WRITE;
+/*!40000 ALTER TABLE `mail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `news_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `time` datetime NOT NULL DEFAULT '1970-01-01 00:00:01',
+  `importance` tinyint(4) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`news_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `online`
+--
+
+DROP TABLE IF EXISTS `online`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `online` (
+  `hash` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `ua` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `refer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastmove` int(10) NOT NULL,
+  `firsttime` int(10) DEFAULT NULL,
+  `uri` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`hash`),
+  UNIQUE KEY `hash` (`hash`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `online`
+--
+
+LOCK TABLES `online` WRITE;
+/*!40000 ALTER TABLE `online` DISABLE KEYS */;
+/*!40000 ALTER TABLE `online` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `privilege`
+--
+
+DROP TABLE IF EXISTS `privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `privilege` (
+  `user_id` char(20) NOT NULL DEFAULT '',
+  `rightstr` char(30) NOT NULL DEFAULT '',
+  `defunct` char(1) NOT NULL DEFAULT 'N'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `privilege`
+--
+
+LOCK TABLES `privilege` WRITE;
+/*!40000 ALTER TABLE `privilege` DISABLE KEYS */;
+INSERT INTO `privilege` VALUES ('admin','administrator','N');
+/*!40000 ALTER TABLE `privilege` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problem`
+--
+
+DROP TABLE IF EXISTS `problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem` (
+  `problem_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `description` text,
+  `input` text,
+  `output` text,
+  `sample_input` text,
+  `sample_output` text,
+  `spj` char(1) NOT NULL DEFAULT '0',
+  `hint` text,
+  `source` varchar(100) DEFAULT NULL,
+  `in_date` datetime DEFAULT NULL,
+  `time_limit` int(11) NOT NULL DEFAULT '0',
+  `memory_limit` int(11) NOT NULL DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `accepted` int(11) DEFAULT '0',
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  PRIMARY KEY (`problem_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problem`
+--
+
+LOCK TABLES `problem` WRITE;
+/*!40000 ALTER TABLE `problem` DISABLE KEYS */;
+INSERT INTO `problem` VALUES (1000,'A+B Problem','<p>Calculate a+b</p>','<p>Two integer a,b (0&lt;=a,b&lt;=10)</p>','<p>Output a+b</p>','1 2','3','0','<p>Q: Where are the input and the output? A: Your program shall always <font color=\"#ff0000\">read input from stdin (Standard Input) and write output to stdout (Standard Output)</font>. For example, you can use \'scanf\' in C or \'cin\' in C++ to read from stdin, and use \'printf\' in C or \'cout\' in C++ to write to stdout. You <font color=\"#ff0000\">shall not output any extra data</font> to standard output other than that required by the problem, otherwise you will get a &quot;Wrong Answer&quot;. User programs are not allowed to open and read from/write to files. You will get a &quot;Runtime Error&quot; or a &quot;Wrong Answer&quot; if you try to do so. Here is a sample solution for problem 1000 using C++/G++:</p>\r\n<pre>\r\n#include &lt;iostream&gt;\r\nusing namespace std;\r\nint  main()\r\n{\r\n    int a,b;\r\n    cin &gt;&gt; a &gt;&gt; b;\r\n    cout &lt;&lt; a+b &lt;&lt; endl;\r\n    return 0;\r\n}</pre>\r\n<p>It\'s important that the return type of main() must be int when you use G++/GCC,or you may get compile error. Here is a sample solution for problem 1000 using C/GCC:</p>\r\n<pre>\r\n#include &lt;stdio.h&gt;\r\n\r\nint main()\r\n{\r\n    int a,b;\r\n    scanf(&quot;%d %d&quot;,&amp;a, &amp;b);\r\n    printf(&quot;%d\\n&quot;,a+b);\r\n    return 0;\r\n}</pre>\r\n<p>Here is a sample solution for problem 1000 using PASCAL:</p>\r\n<pre>\r\nprogram p1000(Input,Output); \r\nvar \r\n  a,b:Integer; \r\nbegin \r\n   Readln(a,b); \r\n   Writeln(a+b); \r\nend.</pre>\r\n<p>Here is a sample solution for problem 1000 using JAVA: Now java compiler is jdk 1.5, next is program for 1000</p>\r\n<pre>\r\nimport java.io.*;\r\nimport java.util.*;\r\npublic class Main\r\n{\r\n            public static void main(String args[]) throws Exception\r\n            {\r\n                    Scanner cin=new Scanner(System.in);\r\n                    int a=cin.nextInt();\r\n                    int b=cin.nextInt();\r\n                    System.out.println(a+b);\r\n            }\r\n}</pre>\r\n<p>Old program for jdk 1.4</p>\r\n<pre>\r\nimport java.io.*;\r\nimport java.util.*;\r\n\r\npublic class Main\r\n{\r\n    public static void main (String args[]) throws Exception\r\n    {\r\n        BufferedReader stdin = \r\n            new BufferedReader(\r\n                new InputStreamReader(System.in));\r\n\r\n        String line = stdin.readLine();\r\n        StringTokenizer st = new StringTokenizer(line);\r\n        int a = Integer.parseInt(st.nextToken());\r\n        int b = Integer.parseInt(st.nextToken());\r\n        System.out.println(a+b);\r\n    }\r\n}</pre>','POJ','2012-10-07 18:53:38',5,32,'N',0,0,0);
+/*!40000 ALTER TABLE `problem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reply`
+--
+
+DROP TABLE IF EXISTS `reply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reply` (
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` varchar(20) NOT NULL,
+  `time` datetime NOT NULL DEFAULT '1970-01-01 00:00:01',
+  `content` text NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `ip` varchar(30) NOT NULL,
+  PRIMARY KEY (`rid`),
+  KEY `author_id` (`author_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reply`
+--
+
+LOCK TABLES `reply` WRITE;
+/*!40000 ALTER TABLE `reply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reply` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `runtimeinfo`
+--
+
+DROP TABLE IF EXISTS `runtimeinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `runtimeinfo` (
+  `solution_id` int(11) NOT NULL DEFAULT '0',
+  `error` text,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `runtimeinfo`
+--
+
+LOCK TABLES `runtimeinfo` WRITE;
+/*!40000 ALTER TABLE `runtimeinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `runtimeinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sim`
+--
+
+DROP TABLE IF EXISTS `sim`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sim` (
+  `s_id` int(11) NOT NULL,
+  `sim_s_id` int(11) DEFAULT NULL,
+  `sim` int(11) DEFAULT NULL,
+  PRIMARY KEY (`s_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sim`
+--
+
+LOCK TABLES `sim` WRITE;
+/*!40000 ALTER TABLE `sim` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sim` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solution`
+--
+
+DROP TABLE IF EXISTS `solution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solution` (
+  `solution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` char(20) NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '0',
+  `memory` int(11) NOT NULL DEFAULT '0',
+  `in_date` datetime NOT NULL DEFAULT '1970-01-01 00:00:01',
+  `result` smallint(6) NOT NULL DEFAULT '0',
+  `language` int(10) unsigned NOT NULL DEFAULT '0',
+  `ip` char(15) NOT NULL,
+  `contest_id` int(11) DEFAULT NULL,
+  `valid` tinyint(4) NOT NULL DEFAULT '1',
+  `num` tinyint(4) NOT NULL DEFAULT '-1',
+  `code_length` int(11) NOT NULL DEFAULT '0',
+  `judgetime` datetime DEFAULT NULL,
+  `pass_rate` decimal(2,2) unsigned NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`solution_id`),
+  KEY `uid` (`user_id`),
+  KEY `pid` (`problem_id`),
+  KEY `res` (`result`),
+  KEY `cid` (`contest_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solution`
+--
+
+LOCK TABLES `solution` WRITE;
+/*!40000 ALTER TABLE `solution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solution` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `source_code`
+--
+
+DROP TABLE IF EXISTS `source_code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `source_code` (
+  `solution_id` int(11) NOT NULL,
+  `source` text NOT NULL,
+  PRIMARY KEY (`solution_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `source_code`
+--
+
+LOCK TABLES `source_code` WRITE;
+/*!40000 ALTER TABLE `source_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `source_code` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `topic`
+--
+
+DROP TABLE IF EXISTS `topic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `topic` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varbinary(60) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `top_level` int(2) NOT NULL DEFAULT '0',
+  `cid` int(11) DEFAULT NULL,
+  `pid` int(11) NOT NULL,
+  `author_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `cid` (`cid`,`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `topic`
+--
+
+LOCK TABLES `topic` WRITE;
+/*!40000 ALTER TABLE `topic` DISABLE KEYS */;
+/*!40000 ALTER TABLE `topic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(100) DEFAULT NULL,
+  `submit` int(11) DEFAULT '0',
+  `solved` int(11) DEFAULT '0',
+  `defunct` char(1) NOT NULL DEFAULT 'N',
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `accesstime` datetime DEFAULT NULL,
+  `volume` int(11) NOT NULL DEFAULT '1',
+  `language` int(11) NOT NULL DEFAULT '1',
+  `password` varchar(32) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `nick` varchar(100) NOT NULL DEFAULT '',
+  `school` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'judge'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-20 17:37:24
